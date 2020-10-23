52367418a16bbab2e7b14789098f4cd6b8556b2a
Nikki Quibin <nikkiquibin@Nikkis-MacBook-Pro.local>
2020-10-22 20:22:33 -0400

    add get_menus and create_menu un-implemented endpoints

--- menus/controller.py
+++ menus/controller.py
@@ -25,3 +25,15 @@ def get_menu(request: Request) -> Dict[str, Any]:
 
     menu = MenuManager().get_menu(menu_id=menu_id, full=full.lower() == 'true')
     return Response(status_code=200, message_body=menu).to_dict()
+
+
+@global_exception
+@parse_http_event
+def get_menus(_: Request) -> Dict[str, Any]:
+    return Response(status_code=501, message_body='Not implemented').to_dict()
+
+
+@global_exception
+@parse_http_event
+def create_menu(_: Request) -> Dict[str, Any]:
+    return Response(status_code=501, message_body='Not implemented').to_dict()
